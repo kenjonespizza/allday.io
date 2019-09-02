5de61ee88b47ff020aafe45694bd864d1d1ebc9d
Ken Jones <kenjwho@gmail.com>
2019-09-02 15:13:43 -0400

    test build

--- site/src/html.js
+++ site/src/html.js
@@ -12,7 +12,7 @@ export default function HTML (props) {
           content='width=device-width, initial-scale=1, shrink-to-fit=no'
         />
         {props.headComponents}
-        <script src='https://kit.fontawesome.com/8714152a25.js' />
+        <script defer src='https://kit.fontawesome.com/8714152a25.js' />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
@@ -20,7 +20,7 @@ export default function HTML (props) {
           This app works best with JavaScript enabled.
         </noscript>
         <div
-          key={`body`}
+          key='body'
           id='___gatsby'
           dangerouslySetInnerHTML={{__html: props.body}}
         />
