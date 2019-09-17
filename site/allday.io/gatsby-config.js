c924b4c0e0e5814b8b15c8c7c0f879f5be5d4ad2
Ken Jones <kenjwho@gmail.com>
2019-09-17 13:43:23 -0400

    Split into Gatsby Theme and add Gatsby blog theme

--- site/allday.io/gatsby-config.js
+++ site/allday.io/gatsby-config.js
@@ -6,10 +6,44 @@ require('dotenv').config({
 // const path = require('path')
 
 module.exports = {
+  siteMetadata: {
+    title: 'Blog by AllDay',
+    name: 'AllDay',
+    siteUrl: 'https://allday.io/blog',
+    description: 'AllDay Blog - Tampa & Charleston web design and development services',
+
+    // important to set the main text that appears in the hero
+    hero: {
+      heading: 'Perspectives on technology, design and business from the team at Narative.',
+      maxWidth: 652
+    },
+    social: [
+      {
+        name: 'twitter',
+        url: 'https://twitter.com/narative'
+      },
+      {
+        name: 'github',
+        url: 'https://github.com/narative'
+      }
+    ]
+  },
   plugins: [
     {
       resolve: 'allday-main-site',
       options: {}
+    },
+    {
+      resolve: '@narative/gatsby-theme-novela',
+      options: {
+        sources: {
+          local: true
+        },
+        authorsPage: true,
+        contentPosts: 'content/posts',
+        contentAuthors: 'content/authors',
+        basePath: 'blog/'
+      }
     },
     {
       resolve: 'gatsby-plugin-manifest',
