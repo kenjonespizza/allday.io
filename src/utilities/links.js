e4c43a1d4a1792e0af5adb95cb235a99829ad36c
Ken Jones <kenjwho@gmail.com>
2021-01-15 17:01:29 -0500

    Fix footer link

--- src/utilities/links.js
+++ src/utilities/links.js
@@ -1,7 +1,7 @@
 export const links = {
-  contact: 'call-design-team',
-  work: 'work',
-  approach: 'web-app-design-approach',
-  whoWeAre: 'design-architects',
+  contact: 'c/all-design-team',
+  work: '/work',
+  approach: '/web-app-design-approach',
+  whoWeAre: '/design-architects'
   // blog: 'design-studio-blog'
 }
